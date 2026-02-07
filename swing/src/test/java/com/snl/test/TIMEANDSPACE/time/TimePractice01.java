d7d397177935c76cd1bee5302ec3ab74811b5f83
snl <1978719130@qq.com>
2026-02-07 15:14:20 +0800

    第七章碰撞测试演示5

--- swing/src/test/java/com/snl/test/TIMEANDSPACE/time/TimePractice01.java
+++ swing/src/test/java/com/snl/test/TIMEANDSPACE/time/TimePractice01.java
@@ -1,10 +1,11 @@
-package com.snl.test.TIMEANDSPACE.space;
+package com.snl.test.TIMEANDSPACE.time;
 
 import com.snl.test.frame.SimpleGameFramePlus;
 import com.snl.test.vwctor.Matrix3x3f;
 import com.snl.test.vwctor.Vector2D;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
@@ -16,6 +17,8 @@ public class TimePractice01 extends SimpleGameFramePlus implements MouseWheelLis
     double r0;
     boolean c0Moving,c0Collision;
     boolean clicked,sDragging;
+    double rot,theta;
+    boolean stopping;
 
     public TimePractice01() throws HeadlessException {
         super();
@@ -28,10 +31,12 @@ public class TimePractice01 extends SimpleGameFramePlus implements MouseWheelLis
         resetPos();
         //创建轴
         r0 = 1;
+        rot = 0;
+        theta = Math.PI / 4;
     }
 
     private void resetPos() {
-        c0Pos = new Vector2D(1,1);
+        c0Pos = new Vector2D(3,3);
     }
 
     @Override
@@ -40,6 +45,11 @@ public class TimePractice01 extends SimpleGameFramePlus implements MouseWheelLis
         //TODO
         clicked = mouseInputEvent.mouseButtonDownOnce(MouseEvent.BUTTON1);
         sDragging = mouseInputEvent.mouseButtonDown(MouseEvent.BUTTON1);
+
+        if (keyBoardEvent.keyDownOnce(KeyEvent.VK_SPACE))
+        {
+            stopping = !stopping;
+        }
     }
 
     @Override
@@ -51,15 +61,31 @@ public class TimePractice01 extends SimpleGameFramePlus implements MouseWheelLis
         Matrix3x3f mat = Matrix3x3f.translate(c0Pos.getX(), c0Pos.getY());
         c0 = mat.mul(new Vector2D());
 
-        if (clicked && pointInCircle(c0,r0))
-            c0Moving = true;
+        if (!stopping) {
+            if (clicked && pointInCircle(c0, r0))
+                c0Moving = true;
 
-        c0Moving = c0Moving && sDragging;
-        // 像素 → 世界单位
-        Matrix3x3f scale = getReverseScaleViewPortMat();
-        Vector2D move = scale.mul(mouseDelta);
-        if (c0Moving)
-            c0Pos = c0Pos.add(move);
+            rot += theta * delta;
+
+            c0Moving = c0Moving && sDragging;
+            // 像素 → 世界单位
+            Matrix3x3f scale = getReverseScaleViewPortMat();
+            Vector2D move = scale.mul(mouseDelta);
+            if (c0Moving)
+                c0Pos = c0Pos.add(move);
+            c0Collision = circleCollision(c0, r0);
+        }
+    }
+
+    private boolean circleCollision(Vector2D c, double r) {
+        //判断左边
+        Matrix3x3f rotate = Matrix3x3f.rotate(rot);
+        Vector2D c1 = rotate.mul(c);
+        Vector2D vx = new Vector2D(c1.getX(),0);
+        Vector2D vy = new Vector2D(0,c1.getY());
+        double d = Math.pow(r,2);
+        return vy.lenSqr() <= d||
+                vx.lenSqr() <= d;
 
     }
 
@@ -80,25 +106,31 @@ public class TimePractice01 extends SimpleGameFramePlus implements MouseWheelLis
         );
         g2.setColor(Color.WHITE);
         //todo
+        g2.drawString("运动模式:[%s]".formatted(!stopping),30,130);
+        g2.drawString("这是一个测试圆周运动的例子，圆以每秒四分之pi运动",30,150);
         drawCircle(g2,c0,r0);
         g2.dispose();
     }
 
     private void drawCircle(Graphics2D g2, Vector2D c, double r) {
+        g2.setColor(c0Collision?Color.red:Color.green);
         Matrix3x3f vt = getViewportTransform();
         Matrix3x3f scale = getScaleViewPortMat();
+        Matrix3x3f rotate = Matrix3x3f.rotate(rot);
+        vt = vt.mul(rotate);
+        Vector2D c1 = rotate.mul(c);
         Vector2D p = vt.mul(c);
         Vector2D v = new Vector2D(2 * r,2 * r);
         v = scale.mul(v);
         //获取坐标
         double w = v.getX();
         double h = v.getY();
-        double centerX = p.getX() - w / 2.0;
-        double centerY = p.getY() - h / 2.0;
+        double leftX = p.getX() - w / 2.0;
+        double leftY = p.getY() - h / 2.0;
         Shape s = new Ellipse2D.Double(
-                centerX,centerY,w,h
+                leftX,leftY,w,h
         );
-        g2.drawString("[%.2f,%.2f]".formatted(c.getX(),c.getY()),
+        g2.drawString("[%.2f,%.2f]".formatted(c1.getX(),c1.getY()),
                 (int) p.getX(), (int) (p.getY() - 10));
         Shape radius = new Ellipse2D.Double(
                 p.getX()-2,p.getY()-2,4,4
